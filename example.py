543c7b214ccf934dbb70d2244a713b032fe156a4
Isuru Adikaram <izuru775@gmail.com>
2022-11-11 17:57:01 +1100

    Implemented a simple aba generator

--- example.py
+++ example.py
@@ -1,27 +1,42 @@
 import datetime
 from aba.generator import AbaFile
 from aba import records
+# 
+import requests
+import json
 
-header = records.DescriptiveRecord(
-    user_bank='NAB',
-    user_name='AJAX CRACKERS',
-    user_number=12345,
-    description='SALARIES',
-    date=datetime.date(day=05, month=02, year=2000)
-)
+# connect to an api get the data
+r = requests.get('https://07db78ea-5dec-4187-af8d-62b87341ca34.mock.pstmn.io/aba')
+data = json.loads(r.text)
 
-entry = records.DetailRecord(
-    bsb='123-456',
-    account_number='123456',
-    txn_code=53,
-    amount=4242,
-    payee_name='HACKER, J. RANDOM',
-    lodgment_ref='RANDOM PAYMENT',
-    sender_bsb='987-654',
-    sender_account='445566777',
-    remitter_name='AJAX CRACKERS',
-)
+newData = data['data']
+#loop through new data and create a new record for each item
+for item in newData:
+    header = records.DescriptiveRecord(
+        user_bank=item['user_bank'],
+        user_name=item['user_name'],
+        user_number=item['user_number'],
+        description=item['description'],
+        date=datetime.date(item['year'],item['month'],item['date'])
+    )
 
-aba_file = AbaFile(header)
-aba_file.add_record(entry)
-print aba_file.render_to_string()
+    entry = records.DetailRecord(
+        bsb=item['entry']['bsb'],
+        account_number=item['entry']['account_number'],
+        txn_code=item['entry']['txn_code'],
+        amount=item['entry']['amount'],
+        payee_name=item['entry']['payee_name'],
+        lodgment_ref=item['entry']['lodgment_ref'],
+        sender_bsb=item['entry']['sender_bsb'],
+        sender_account=item['entry']['sender_account'],
+        remitter_name=item['entry']['remitter_name'],
+    )
+
+    aba_file = AbaFile(header)
+    aba_file.add_record(entry)
+
+    print (aba_file.render_to_string())
+    #  Create file/ Write to file
+    f = open("myfile.txt", "a")
+    f.write(aba_file.render_to_string()+"\r")
+    f.close()
